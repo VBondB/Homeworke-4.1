12cb8f81a10b95748064341f60b0e079bf733484
VBondB <Bvv051270@gmail.com>
2025-01-30 23:37:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введіть номер дня тижня (1-7): ");
+int dayNumber = int.Parse(Console.ReadLine());
+
+string dayName;
+if (dayNumber == 1)
+{
+    dayName = "понеділок";
+}
+else if (dayNumber == 2)
+{
+    dayName = "вівторок";
+}
+else if (dayNumber == 3)
+{
+    dayName = "середа";
+}
+else if (dayNumber == 4)
+{
+    dayName = "четвер";
+}
+else if (dayNumber == 5)
+{
+    dayName = "п'ятниця";
+}
+else if (dayNumber == 6)
+{
+    dayName = "субота";
+}
+else if (dayNumber == 7)
+{
+    dayName = "неділя";
+}
+else
+{
+    dayName = "Неправильний номер дня тижня";
+}
+
+Console.WriteLine("Назва дня тижня: " + dayName);
+
